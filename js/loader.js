1ae0ac26973453240e47a8cc90e17e0e0f38ab91
Teddarific <teddy.ni.19@dartmouth.edu>
2017-06-13 11:51:28 -0400

    loader

--- js/loader.js
+++ js/loader.js
@@ -1,6 +1,11 @@
 function loader(){
   setTimeout(dispLogo,500);
-  setTimeout(loaded,6700);
+  setTimeout(transition,6700);
+  setTimeout(loaded,8700);
+}
+
+function transition(){
+  document.getElementById("logo-wrapper").classList.add("chgColour");
 }
 
 function loaded(){
@@ -15,11 +20,21 @@ function dispLogo(){
 
     var size = document.documentElement.clientWidth * 0.11;
 
+    var col1 = '#985E6D';
+    var col2 = '#494E6B';
     var options = {
       size: size,         // Font size, defined by the height of the letters (pixels)
       weight: 5,         // Font weight (pixels)
       rounded: true,    // Rounded letter endings
-      color: ['#985E6D', '#FFFFFF'],  // Font color
+      color: [col1, //T
+              col2, //E
+              col2, //D
+              col2, //D
+              col2, //Y
+              col2, //<space>
+              col1, //N
+              col2  //I
+             ],
       duration: 1.3,       // Duration of the animation of each letter (seconds)
       delay: [0.1, 0.3],  // Delay animation among letters (seconds)
       fade: 0.5,         // Fade effect duration (seconds)
