b9986a924b981a022ac7abf46b0a7306991605ae
Teddarific <teddy.ni.19@dartmouth.edu>
2017-06-13 00:10:14 -0400

    initial animation

--- js/facts.js
+++ js/facts.js
@@ -1,53 +1,3 @@
-
-//TYPER FUNCTION ORIGINAL INSPIRATION SOURCE: http://www.bootply.com/rSd1F5vUaM
-//ORIGINAL VERSION CREATED BY tjgrendel
-//Modified version created by Teddy Ni
-$.fn.typer = function(text, options){
-
-    //Options
-    options = $.extend({}, {
-        char: ' ',
-        delay: 2000, //Delay between switching facts
-        duration: 1000, //how long each fact lasts
-        endless: true
-    }, options || text);
-
-    text = $.isPlainObject(text) ? options.text : text;
-
-    var elem = $(this),
-        isTag = false,
-        c = 0;
-
-    //i is fact number
-    (function typetext(i) {
-        var e = ({string:1, number:1}[typeof text] ? text : text[i]) + options.char,
-            char = e.substr(c++, 1);
-
-        if( char === '<' ){ isTag = true; }
-        if( char === '>' ){ isTag = false; }
-        elem.html(e.substr(0, c));
-        if(c <= e.length){
-            if( isTag ){
-                typetext(i);
-            } else {
-                setTimeout(typetext, options.duration/10, i);
-            }
-        } else {
-            c = 0;
-            //Find random fact instead:
-            var k;
-            do{
-              k = Math.floor((Math.random() * text.length));
-            }
-            while( i == k) //Ensure a different fact each time
-            i = k;
-
-            setTimeout(typetext, options.delay, i);
-        }
-    })(0);
-};
-
-
 $(document).ready(function(){
   $(function(){
       $("#fact").typed({
@@ -63,18 +13,4 @@ $(document).ready(function(){
               typeSpeed: 50
             });
       });
-
-  /*
-  $('#fact').typer([
-                    "is from Rhode Island",
-                    "can solve a rubik's cube",
-                    "is a member of Sigma Phi Epsilon",
-                    "Developer",
-                    "Designer",
-                    "Thinker",
-                    "Entrepreneur",
-                    "Student",
-                    "Learner",
-                  ]);
-                */
 });
