21b0fafb767990c390d0b5f04255e27be11bfbc8
Teddarific <teddy.ni.19@dartmouth.edu>
2017-12-21 10:25:29 -0500

    structure with video in place

--- js/facts.js
+++ js/facts.js
@@ -3,25 +3,37 @@ $(document).ready(function(){
     var lab = document.getElementById('name');
     var triggered = {};
 
+    var options = {
+      strings: [
+                "a developer.",
+                "a learner.",
+                "a designer.",
+                "a thinker.",
+                "an explorer."
+               ],
+         typeSpeed: 50,
+         startDelay: 0,
+    };
+    var controller = new Typed($("#fact")[0], options);
+
     vid.addEventListener('timeupdate',function(event){
       var time = parseInt(vid.currentTime);
       // lab.innerHTML = parseInt(vid.currentTime);
 
-      if ( time == 3 && triggered[time] === undefined){
-        triggered[time] = true;
-        var options = {
-          strings: [
-                    "a developer."
-                   ],
-             typeSpeed: 50,
-             loop: false,
-        };
-        var controller = new Typed($("#fact")[0], options);
-        var execBack = function(){
-          controller.backspace("a developer.", 5);
-        }
-        setTimeout(execBack, 5000);
+      // reset what we've seen
+      if ( time == 0 ){
+        triggered = {};
       }
+      if ( time == 0 && triggered[time] === undefined && time != 30){
+        triggered[time] = true;
+        controller.next();
+
+        // var execBack = function(){
+        //   controller.backspace("a developer.", 5);
+        // }
+        // setTimeout(execBack, 5000);
+      }
+
     },false);
 
     // setTimeout(startTyping,1000)
