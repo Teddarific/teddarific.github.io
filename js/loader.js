997b20d5e46f273cb873f4889ee9f4b9d3c22bf9
Teddarific <teddy.ni.19@dartmouth.edu>
2017-12-21 19:16:35 -0500

    buffered loader

--- js/loader.js
+++ js/loader.js
@@ -5,29 +5,44 @@ $(document).ready(function(){
 
 
 function preloadVideo(){
-  console.log("Requesting background video...")
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'tedall.mp4', true);
-  xhr.responseType = 'blob';
-  xhr.onload = function(e) {
-    if (this.status == 200) {
-      console.log("Received video...");
-      var myBlob = this.response;
-      var vid = (window.webkitURL ? webkitURL : URL).createObjectURL(myBlob);
-      // myBlob is now the blob that the object URL pointed to.
-      var video = document.getElementById("backvid");
-      console.log("Loading video into element...");
-      video.src = vid;
+  // console.log("Requesting background video...")
+  // var xhr = new XMLHttpRequest();
+  // xhr.open('GET', 'tedall.mp4', true);
+  // xhr.responseType = 'blob';
+  // xhr.onload = function(e) {
+  //   if (this.status == 200) {
+  //     console.log("Received video...");
+  //     var myBlob = this.response;
+  //     var vid = (window.webkitURL ? webkitURL : URL).createObjectURL(myBlob);
+  //     // myBlob is now the blob that the object URL pointed to.
+  //     var video = document.getElementById("backvid");
+  //     console.log("Loading video into element...");
+  //     video.src = vid;
+  //     transition();
+  //     initiateFacts();
+  //    }
+  //    else {
+  //      console.log("Failed to load video");
+  //      // insert code to deal with not loading video
+  //    }
+  //   }
+  //
+  // xhr.send();
+
+  var video = document.getElementById("backvid");
+
+  var ptracker = function() {
+    // console.log(Math.round(video.buffered.end(0)));
+    // When buffer is 1 whole video is buffered
+    if (Math.round(video.buffered.end(0)) >= 25) {
+      video.currentTime = 0;
+      video.removeEventListener("progress", ptracker, false);
       transition();
       initiateFacts();
-     }
-     else {
-       console.log("Failed to load video");
-       // insert code to deal with not loading video
-     }
-    }
+   }
+  }
 
-  xhr.send();
+  video.addEventListener("progress", ptracker, false);
 }
 
 function transition(){
