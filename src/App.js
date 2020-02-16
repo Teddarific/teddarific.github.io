a041470e98221caf52f850b2a2e74f9b5fc010eb
Teddarific <teddy.ni.19@dartmouth.edu>
2020-02-16 13:13:37 -0800

    [v3] Migrating site to React

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+import Test from './components/Test'
+
+export default function App() {
+  return (
+    <div>
+      <Test />
+    </div>
+  )
+}
---
+++ src/components/Test.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export default function Test(){
+  return (
+    <div> A new test component </div>
+  )
+}
